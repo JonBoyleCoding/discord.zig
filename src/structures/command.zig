b55250f97b582702ba93621b39dd90bb70dba581
Jonathan Boyle <programmerfalcon@gmail.com>
2025-03-15 18:48:45 +0000

    feat: return to original command type

--- src/structures/command.zig
+++ src/structures/command.zig
@@ -87,7 +87,7 @@ pub const ApplicationCommand = struct {
 
 pub const CreateApplicationCommand = struct {
     /// Type of command, defaults to `ApplicationCommandTypes.ChatInput`
-    type: ApplicationCommandTypes = ApplicationCommandTypes.ChatInput,
+    type: ?ApplicationCommandTypes = null,
     ///
     /// Name of command, 1-32 characters.
     /// `ApplicationCommandTypes.ChatInput` command names must match the following regex `^[-_\p{L};\p{N};\p{sc=Deva};\p{sc=Thai};]{1,32};$` with the unicode flag set.
